5fb9424d3ab16394baab1d0342922eff510a2999
ervl82 <89858293+ervl82@users.noreply.github.com>
2023-01-19 18:55:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 oauth_source ='https://www.section.io/engineering-education/spotify-python-part-1/'
 docs = 'https://spotipy.readthedocs.io/en/2.19.0/?highlight=get_access_token#spotipy.oauth2.SpotifyOAuth.get_access_token'
-
-
-# In[2]:
 
 
 # import relevant libraries
@@ -18,23 +9,8 @@ from secrets import * # secrets.py file contains clientId, clientSecret, redirec
 import spotipy # to use spotipy library functions
 from spotipy.oauth2 import SpotifyOAuth # to get Access Token
 
-
-# In[3]:
-
-
 # define script access scope
 scope = "playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public user-library-read user-library-modify user-top-read user-read-recently-played"
-
-
-# In[4]:
-
-
-# Code to get token to use spotipy functions
-# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientId, client_secret= clientSecret, redirect_uri=redirect_url, scope=scope))
-
-
-# In[5]:
-
 
 # define oauth object and get access token
 sp_oauth = SpotifyOAuth( clientId, clientSecret,redirect_url2,scope=scope)
@@ -49,7 +25,7 @@ if token_info:
     print('Access Token: '+ spotify_token )
 else:
     print('Cached token not found. Getting Access Token')
-    token_info = sp_oauth.get_access_token(as_dict=False, open_browser=False)
+    token_info = sp_oauth.get_access_token(as_dict=False)
     spotify_token = token_info
     print('Access Token: '+ spotify_token)
 
@@ -63,14 +39,8 @@ else:
     print('No user information available')
 
 
-# In[6]:
-
-
 # set Playlists ids from Spotify
 buongiorno = "6gp8NsLGHOpldIqBUukJQV"
-
-
-# In[7]:
 
 
 # get playlist current episodes and store in a delete list to delete them later
@@ -90,9 +60,6 @@ for item in playlist_json_items:
     delete_episode_uri = item['track']['uri']
     delete_episode_uri_list.append(delete_episode_uri)
 print(delete_episode_uri_list)
-
-
-# In[8]:
 
 
 #remove episodes in the delete list from playlist and print delete confirmations as snapshot_ids
@@ -117,9 +84,6 @@ for delete_episode in delete_episode_uri_list:
     playlist_remove_json
 
 
-# In[9]:
-
-
 # set shows ids from Spotify and print list
 in4minuti = "33YWzJrR8RkFFdocLDSc3c"
 the_essential = "43A9fUmUbLYaHKSi1lAtn5"
@@ -129,9 +93,6 @@ shows_list.append(in4minuti)
 shows_list.append(the_essential)
 shows_list.append(start)
 shows_list
-
-
-# In[10]:
 
 
 #get shows episodes and store latest episode uri for each show into a list, print episodes uri list
@@ -152,10 +113,6 @@ for show in shows_list:
     
 print(episodes_uri_list)
 
-
-# In[11]:
-
-
 #add episodes to playlist based on uri value stored in episodes uri list, and print add confirmations as snapshot_ids
 for episode_uri in episodes_uri_list:
     playlist_add_url = f"https://api.spotify.com/v1/playlists/{buongiorno}/tracks?uris={episode_uri}"
@@ -170,13 +127,7 @@ for episode_uri in episodes_uri_list:
     playlist_add_json
 
 
-# In[12]:
-
-
 print('Automation script has finished')
-
-
-# In[ ]:
 
 
 
