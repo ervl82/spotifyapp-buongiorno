5b84f850b0eb79f31e318e827eecc9b5038a0b53
ervl82 <89858293+ervl82@users.noreply.github.com>
2023-01-19 18:37:47 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -2,3 +2,5 @@ clientId = "054c01d370984919a79aeb4166de8c7f"
 clientSecret = "5d6a0e12f31846a7ab5951d54d4c862d" 
 redirect_url='http://localhost:9000'
 redirect_url2='https://spotifyappervl8.herokuapp.com/callback'
+sp_dc = "AQBAM5DZ2enogDSWWBa8R_WzSPDJQLduOqMgww85qUHR520XG_hTnC-hCbaH3CAEG59FuJF_ynNS-XZV1Rlpc5c-0tBS3npSBcwFVGESUeihLbEb1qIBTBfZGE_xB-yzpUeqTGm2E7G5JCWZTrTBj4j2RjMh2RM"
+sp_key = "6c2912fc-4d98-493f-902f-6999f6731a39"
