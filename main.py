b4db0d8ded472f48a0236a73bb23f93acdcb4282
ervl82 <89858293+ervl82@users.noreply.github.com>
2023-01-25 17:32:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,129 +31,129 @@ SPOTIFY_CLIENT_REFRESH_TOKEN = os.getenv('SPOTIFY_CLIENT_REFRESH_TOKEN')
 
 AUTH_SCOPE = "playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public user-library-read user-library-modify user-top-read user-read-recently-played"
 
-# Define base url for authorization site and define response type
-auth_url_base = 'https://accounts.spotify.com/authorize?'
-response_type = 'code'
+# # Define base url for authorization site and define response type
+# auth_url_base = 'https://accounts.spotify.com/authorize?'
+# response_type = 'code'
 
-# Create payload based on data from config file extracted during __init__
-payload = {
-  'client_id': SPOTIFY_CLIENT_ID,                   
-  'redirect_uri': SPOTIFY_CLIENT_CALLBACK_URL,                   
-  'response_type': response_type,                   
-  'scope': AUTH_SCOPE
-}
+# # Create payload based on data from config file extracted during __init__
+# payload = {
+#   'client_id': SPOTIFY_CLIENT_ID,                   
+#   'redirect_uri': SPOTIFY_CLIENT_CALLBACK_URL,                   
+#   'response_type': response_type,                   
+#   'scope': AUTH_SCOPE
+# }
 
-# Encode the payload for a url and append to the base url
-url_params = urllib.parse.urlencode(payload)
-full_auth_url = auth_url_base + url_params
-print('Custom authorization url:', full_auth_url, '\n')
+# # Encode the payload for a url and append to the base url
+# url_params = urllib.parse.urlencode(payload)
+# full_auth_url = auth_url_base + url_params
+# print('Custom authorization url:', full_auth_url, '\n')
 
-print('Directing to Spotify Authorization page...')
+# print('Directing to Spotify Authorization page...')
 
-# Create selenium options
-options = FirefoxOptions()
-options.add_argument("--headless")
+# # Create selenium options
+# options = FirefoxOptions()
+# options.add_argument("--headless")
 
-# Create selenium driver
-driver = webdriver.Firefox(
-  service = FirefoxService(GeckoDriverManager().install()),
-  options = options
-)
+# # Create selenium driver
+# driver = webdriver.Firefox(
+#   service = FirefoxService(GeckoDriverManager().install()),
+#   options = options
+# )
 
-# Open browser
-driver.get(full_auth_url)
+# # Open browser
+# driver.get(full_auth_url)
 
-# Find html form fields
-login_field = driver.find_element(By.ID, 'login-username')
-pass_field = driver.find_element(By.ID, 'login-password')
-sub_button = driver.find_element(By.ID, 'login-button')
+# # Find html form fields
+# login_field = driver.find_element(By.ID, 'login-username')
+# pass_field = driver.find_element(By.ID, 'login-password')
+# sub_button = driver.find_element(By.ID, 'login-button')
 
-print('Submitting user login data...\n')
+# print('Submitting user login data...\n')
 
-print(f"Username ({SPOTIFY_USERNAME}) length of {len(SPOTIFY_USERNAME)}")
-print(f"Password (******) length of {len(SPOTIFY_PASSWORD)}")
+# print(f"Username ({SPOTIFY_USERNAME}) length of {len(SPOTIFY_USERNAME)}")
+# print(f"Password (******) length of {len(SPOTIFY_PASSWORD)}")
 
-# Pass user data to form
-login_field.send_keys(SPOTIFY_USERNAME)
-pass_field.send_keys(SPOTIFY_PASSWORD)
+# # Pass user data to form
+# login_field.send_keys(SPOTIFY_USERNAME)
+# pass_field.send_keys(SPOTIFY_PASSWORD)
 
-print('Sent keys...\n')
+# print('Sent keys...\n')
 
-# Selenium raises an error when you can't connect to a domain
-# Catch that error to handle it and extract the URL for the access token.
-try:
-  # Submit the form
-  sub_button.click()
-  print('sub_button click...\n')
+# # Selenium raises an error when you can't connect to a domain
+# # Catch that error to handle it and extract the URL for the access token.
+# try:
+#   # Submit the form
+#   sub_button.click()
+#   print('sub_button click...\n')
 
-  # pause for redirect to acceptance page
-  time.sleep(3)
+#   # pause for redirect to acceptance page
+#   time.sleep(3)
 
-  # Find agree button
-  # driver.find_element_by_id('auth-accept')
-  agree_button = driver.find_element('[data-testid="auth-accept"]')
-  print('Button Found')
+#   # Find agree button
+#   # driver.find_element_by_id('auth-accept')
+#   agree_button = driver.find_element('[data-testid="auth-accept"]')
+#   print('Button Found')
 
-  # Click button and wait for error
-  agree_button.click()
-  print('Button Clicked')
-  time.sleep(30)
-except Exception as err:
-  # Catch error and do nothing to keep code going
-  print('Error caught')
-  print('Redirect successful...\n')
-  print('Extracting authorization code now...\n')
+#   # Click button and wait for error
+#   agree_button.click()
+#   print('Button Clicked')
+#   time.sleep(30)
+# except Exception as err:
+#   # Catch error and do nothing to keep code going
+#   print('Error caught')
+#   print('Redirect successful...\n')
+#   print('Extracting authorization code now...\n')
 
-time.sleep(5)
+# time.sleep(5)
 
-# Get current url which now contains the access token 
-redirect = driver.current_url
-driver.close()
+# # Get current url which now contains the access token 
+# redirect = driver.current_url
+# driver.close()
 
-print('Redirect-URI:', redirect)
+# print('Redirect-URI:', redirect)
 
-parsed = urllib.parse.urlparse(redirect)
-return_package = urllib.parse.parse_qs(parsed.query)
+# parsed = urllib.parse.urlparse(redirect)
+# return_package = urllib.parse.parse_qs(parsed.query)
 
-# Return the query from the rediret url
-AUTH_CODE = return_package['code'][0]
+# # Return the query from the rediret url
+# AUTH_CODE = return_package['code'][0]
 
-# Generate body for request to get access token
-grant_type = 'authorization_code'
+# # Generate body for request to get access token
+# grant_type = 'authorization_code'
 
-body = {
-  'grant_type': grant_type,
-  'code': AUTH_CODE,
-  'redirect_uri': SPOTIFY_CLIENT_CALLBACK_URL
-}
+# body = {
+#   'grant_type': grant_type,
+#   'code': AUTH_CODE,
+#   'redirect_uri': SPOTIFY_CLIENT_CALLBACK_URL
+# }
 
-# Format client id and secret for request header + encode them
-client_params = SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
-encoded_client_params = base64.b64encode(client_params.encode('ascii'))
+# # Format client id and secret for request header + encode them
+# client_params = SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
+# encoded_client_params = base64.b64encode(client_params.encode('ascii'))
 
-print('\nLoading Client Secret, Client ID, and Authorization Pay Load...')
+# print('\nLoading Client Secret, Client ID, and Authorization Pay Load...')
 
-# Create header with encoded client id and secret
-headers = {'Authorization': 'Basic ' + encoded_client_params.decode('ascii')}
+# # Create header with encoded client id and secret
+# headers = {'Authorization': 'Basic ' + encoded_client_params.decode('ascii')}
 
-print('\nRequesting Access and Refresh Tokens...')
+# print('\nRequesting Access and Refresh Tokens...')
 
-# Submit post request to get the access tokens
-response = requests.post(
-  'https://accounts.spotify.com/api/token',
-  data = body,
-  headers = headers
-)
+# # Submit post request to get the access tokens
+# response = requests.post(
+#   'https://accounts.spotify.com/api/token',
+#   data = body,
+#   headers = headers
+# )
 
-tokens = response.text
-tokens_parsed = json.loads(tokens)
+# tokens = response.text
+# tokens_parsed = json.loads(tokens)
 
-print('\nParsing and extracting tokens...')
-SPOTIFY_CLIENT_TOKEN = tokens_parsed['access_token']
-SPOTIFY_CLIENT_REFRESH_TOKEN = tokens_parsed['refresh_token']
+# print('\nParsing and extracting tokens...')
+# SPOTIFY_CLIENT_TOKEN = tokens_parsed['access_token']
+# SPOTIFY_CLIENT_REFRESH_TOKEN = tokens_parsed['refresh_token']
 
-print(SPOTIFY_CLIENT_TOKEN)
-print(SPOTIFY_CLIENT_REFRESH_TOKEN)
+# print(SPOTIFY_CLIENT_TOKEN)
+# print(SPOTIFY_CLIENT_REFRESH_TOKEN)
 
 
 grant_type2 = 'refresh_token'
