106438beaf84bbe48ec86746c752a1dabf24010c
ervl82 <89858293+ervl82@users.noreply.github.com>
2023-01-25 17:16:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,187 @@
-oauth_source ='https://www.section.io/engineering-education/spotify-python-part-1/'
-docs = 'https://spotipy.readthedocs.io/en/2.19.0/?highlight=get_access_token#spotipy.oauth2.SpotifyOAuth.get_access_token'
-
-
-# import relevant libraries
-import json # to handle API requests outputs
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy.util as util
+import pandas as pd
+from pathlib import Path
+import json
 import requests # to make requests to API
-from secrets import * # secrets.py file contains clientId, clientSecret, redirect_url, redirect_url2
-import spotipy # to use spotipy library functions
-from spotipy.oauth2 import SpotifyOAuth # to get Access Token
+import sys
+import urllib.parse
+import base64
+import time
 
-# define script access scope
-scope = "playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public user-library-read user-library-modify user-top-read user-read-recently-played"
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-# define oauth object and get access token
-sp_oauth = SpotifyOAuth( clientId, clientSecret,redirect_url2,scope=scope)
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
-spotify_token = ""
+load_dotenv()
 
-token_info = sp_oauth.get_cached_token()
+sp = None
 
-if token_info:
-    print('Found cached token!')
-    spotify_token = token_info['access_token']
-    print('Access Token: '+ spotify_token )
-else:
-    print('Cached token not found. Getting Access Token')
-    token_info = sp_oauth.get_access_token(as_dict=False)
-    spotify_token = token_info
-    print('Access Token: '+ spotify_token)
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_CLIENT_CALLBACK_URL = 'http://localhost:8888/callback'
+SPOTIFY_USERNAME = os.getenv('SPOTIFY_USERNAME')
+SPOTIFY_PASSWORD = os.getenv('SPOTIFY_PASSWORD')
+SPOTIFY_CLIENT_REFRESH_TOKEN = os.getenv('SPOTIFY_CLIENT_REFRESH_TOKEN')
 
-if spotify_token:
-    print('Access token available! Trying to get user information...')
-    sp = spotipy.Spotify(spotify_token)
-    results = sp.current_user()
-    print('User: '+ results['id'])
+AUTH_SCOPE = "playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public user-library-read user-library-modify user-top-read user-read-recently-played"
 
-else:
-    print('No user information available')
+# Define base url for authorization site and define response type
+auth_url_base = 'https://accounts.spotify.com/authorize?'
+response_type = 'code'
 
+# Create payload based on data from config file extracted during __init__
+payload = {
+  'client_id': SPOTIFY_CLIENT_ID,                   
+  'redirect_uri': SPOTIFY_CLIENT_CALLBACK_URL,                   
+  'response_type': response_type,                   
+  'scope': AUTH_SCOPE
+}
+
+# Encode the payload for a url and append to the base url
+url_params = urllib.parse.urlencode(payload)
+full_auth_url = auth_url_base + url_params
+print('Custom authorization url:', full_auth_url, '\n')
+
+print('Directing to Spotify Authorization page...')
+
+# Create selenium options
+options = FirefoxOptions()
+# options.add_argument("--headless")
+
+# Create selenium driver
+driver = webdriver.Firefox(
+  service = FirefoxService(GeckoDriverManager().install()),
+  options = options
+)
+
+# Open browser
+driver.get(full_auth_url)
+
+# Find html form fields
+login_field = driver.find_element(By.ID, 'login-username')
+pass_field = driver.find_element(By.ID, 'login-password')
+sub_button = driver.find_element(By.ID, 'login-button')
+
+print('Submitting user login data...\n')
+
+print(f"Username ({SPOTIFY_USERNAME}) length of {len(SPOTIFY_USERNAME)}")
+print(f"Password (******) length of {len(SPOTIFY_PASSWORD)}")
+
+# Pass user data to form
+login_field.send_keys(SPOTIFY_USERNAME)
+pass_field.send_keys(SPOTIFY_PASSWORD)
+
+print('Sent keys...\n')
+
+# Selenium raises an error when you can't connect to a domain
+# Catch that error to handle it and extract the URL for the access token.
+try:
+  # Submit the form
+  sub_button.click()
+  print('sub_button click...\n')
+
+  # pause for redirect to acceptance page
+  time.sleep(3)
+
+  # Find agree button
+  # driver.find_element_by_id('auth-accept')
+  agree_button = driver.find_element('[data-testid="auth-accept"]')
+  print('Button Found')
+
+  # Click button and wait for error
+  agree_button.click()
+  print('Button Clicked')
+  time.sleep(30)
+except Exception as err:
+  # Catch error and do nothing to keep code going
+  print('Error caught')
+  print('Redirect successful...\n')
+  print('Extracting authorization code now...\n')
+
+time.sleep(5)
+
+# Get current url which now contains the access token 
+redirect = driver.current_url
+driver.close()
+
+print('Redirect-URI:', redirect)
+
+parsed = urllib.parse.urlparse(redirect)
+return_package = urllib.parse.parse_qs(parsed.query)
+
+# Return the query from the rediret url
+AUTH_CODE = return_package['code'][0]
+
+# Generate body for request to get access token
+grant_type = 'authorization_code'
+
+body = {
+  'grant_type': grant_type,
+  'code': AUTH_CODE,
+  'redirect_uri': SPOTIFY_CLIENT_CALLBACK_URL
+}
+
+# Format client id and secret for request header + encode them
+client_params = SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
+encoded_client_params = base64.b64encode(client_params.encode('ascii'))
+
+print('\nLoading Client Secret, Client ID, and Authorization Pay Load...')
+
+# Create header with encoded client id and secret
+headers = {'Authorization': 'Basic ' + encoded_client_params.decode('ascii')}
+
+print('\nRequesting Access and Refresh Tokens...')
+
+# Submit post request to get the access tokens
+response = requests.post(
+  'https://accounts.spotify.com/api/token',
+  data = body,
+  headers = headers
+)
+
+tokens = response.text
+tokens_parsed = json.loads(tokens)
+
+print('\nParsing and extracting tokens...')
+SPOTIFY_CLIENT_TOKEN = tokens_parsed['access_token']
+SPOTIFY_CLIENT_REFRESH_TOKEN = tokens_parsed['refresh_token']
+
+print(SPOTIFY_CLIENT_TOKEN)
+print(SPOTIFY_CLIENT_REFRESH_TOKEN)
+
+
+grant_type2 = 'refresh_token'
+body2 = {
+  'grant_type':grant_type2,
+  'refresh_token': SPOTIFY_CLIENT_REFRESH_TOKEN
+}
+
+client_params2 = SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
+encoded_client_params2 = base64.b64encode(client_params2.encode('ascii'))
+headers2 = {'Authorization': 'Basic ' + encoded_client_params2.decode('ascii')}
+
+response2 = requests.post(
+  'https://accounts.spotify.com/api/token',
+  data = body2,
+  headers = headers2
+)
+
+tokens2 = response2.text
+tokens_parsed2 = json.loads(tokens2)
+
+print('\nParsing and extracting tokens...')
+SPOTIFY_CLIENT_TOKEN = tokens_parsed2['access_token']
+# SPOTIFY_CLIENT_REFRESH_TOKEN = tokens_parsed2['refresh_token']
+
+print(SPOTIFY_CLIENT_TOKEN)
+# print(SPOTIFY_CLIENT_REFRESH_TOKEN)
 
 # set Playlists ids from Spotify
 buongiorno = "6gp8NsLGHOpldIqBUukJQV"
@@ -49,7 +193,7 @@ playlist_response = requests.get(
         playlist_url,
         headers ={
             "Content_Type": "application/json", 
-            "Authorization" : f"Bearer {spotify_token}"
+            "Authorization" : f"Bearer {SPOTIFY_CLIENT_TOKEN}"
         }
     )
 playlist_json = playlist_response.json()
@@ -70,7 +214,7 @@ for delete_episode in delete_episode_uri_list:
         headers ={
             "Accept": "application/json",
             "Content_Type": "application/json", 
-            "Authorization" : f"Bearer {spotify_token}"
+            "Authorization" : f"Bearer {SPOTIFY_CLIENT_TOKEN}"
         },
         json = {
             'tracks': [
@@ -103,7 +247,7 @@ for show in shows_list:
         episode_url,
         headers ={
             "Content_Type": "application/json", 
-            "Authorization" : f"Bearer {spotify_token}"
+            "Authorization" : f"Bearer {SPOTIFY_CLIENT_TOKEN}"
         }
     )
     episodes_json = episode_response.json()
@@ -120,7 +264,7 @@ for episode_uri in episodes_uri_list:
         playlist_add_url,
         headers ={
             "Content_Type": "application/json", 
-            "Authorization" : f"Bearer {spotify_token}"
+            "Authorization" : f"Bearer {SPOTIFY_CLIENT_TOKEN}"
         }
     )
     playlist_add_json = playlist_add_response.json()
@@ -128,7 +272,3 @@ for episode_uri in episodes_uri_list:
 
 
 print('Automation script has finished')
-
-
-
-
